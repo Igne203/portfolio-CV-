1882bd88e2f7f79204ac693d8766cc566a065595
Ignė Viktorija <igne.bagdonaite@stud.techin.lt>
2026-04-20 20:55:02 +0300

    updated ContactsPage layout and styling

--- src/pages/Contacts/ContactsPage.jsx
+++ src/pages/Contacts/ContactsPage.jsx
@@ -2,11 +2,16 @@ import "./ContactsPage.css";
 import ContactsComponent from "../../components/Footer/Contacts/ContactsComponent";
 
 function ContactsPage() {
+  const baseUrl = import.meta.env.BASE_URL;
+
   return (
     <>
       <div className="contacts block">
         <h1>CONTACTS</h1>
-        <p>Ignė Vikorija Bagdonaitė</p>
+        <div className="contacts-content">
+          <img src={`${baseUrl}assets/portret1.jpg`} alt="Portfolio portrait" />
+          <p>Feel free to reach out!</p>
+        </div>
       </div>
       <ContactsComponent />
     </>
